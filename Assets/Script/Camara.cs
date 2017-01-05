6e4112b6b0525969e504a6b228735f884f8e1403
NickZennin <nicolas.card97@gmail.com>
2017-01-05 15:56:54 -0500

    Inplementacion del menu
    Creacion del menu y adicion de mas escenas

---
+++ Assets/Script/Camara.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camara : MonoBehaviour {
+
+	//Camaras
+	public Camara Menu;
+	public Camara Instruccions;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//OnClick ();
+	}
+
+	// Cambio de camara
+	public void OnClick(){
+		Menu.enabled = false;
+		Instruccions.enabled = true;
+	}
+}
---
+++ Assets/Script/Start.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour {
+		
+	public void StartLoad (int number) {
+		SceneManager.LoadScene (number);
+	}
+
+	public void ExitLoad() {
+		Application.Quit ();
+	}
+}
